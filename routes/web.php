521bb55e56edf93d5443dbb115bea9380e0b80f1
sunilgitb <84218306+sunilgitb@users.noreply.github.com>
2023-12-26 12:05:42 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -46,11 +46,16 @@ Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
 });
 Route::get('users-list', [UserController::class, 'index']);
 
-Route::get('products-list', [ProductController::class, 'index'])->name('products.index');;
-Route::get('add-product', [ProductController::class, 'create']);
-Route::post('store-product', [ProductController::class, 'store'])->name('store-product');
-Route::get('edit-product', [ProductController::class, 'edit']);
-Route::get('delete-product', [ProductController::class, 'delete']);
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::post('/products', [ProductController::class, 'store']);
+
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::put('/products/{id}', [ProductController::class, 'update']);
+
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
 
 
